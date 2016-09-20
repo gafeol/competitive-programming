1aa4c5532c3ae89b913bed74c580d650322c7d83
Gabriel Fernandes <gabfeol@gmail.com>
2016-09-20 08:59:24 -0300

    Lomsat gelral

--- ojs/cf/600/E.cpp
+++ ojs/cf/600/E.cpp
@@ -14,50 +14,44 @@ inline ll mod(ll x) { return x % modn; }
 const int MAXN = 112345;
 
 int n, m, k;
-int c[MAXN], ind[MAXN], deg;
+int ind[MAXN], deg;
+ll c[MAXN];
 
 vector<int> adj[MAXN];
 
-map<int, int> fc[MAXN], fq[MAXN];
+map<int, int> fc[MAXN];
+map<int, ll> fq[MAXN];
 
-int res[MAXN];
+ll res[MAXN];
 
 void go(int v, int p){
 	ind[v] = deg++;
 	fc[ind[v]][c[v]] = 1;
 	fq[ind[v]][1] = c[v];
-	int mx = 1;
 	for(int nxt: adj[v]){
 		if(nxt == p) continue;
 		go(nxt, v);
 		if(fc[ind[nxt]].size() > fc[ind[v]].size())
 			swap(ind[nxt], ind[v]);
-	//	printf("junta de %d para %d\n", ind[nxt], ind[v]);
 		for(auto& x: fc[ind[nxt]]){
 			if(x.snd == 0) continue;
 			int cor = x.fst;
 			int qtd = x.snd;
-	//		printf("junto cor %d em qtd %d\n", cor, qtd);
 			if(fc[ind[v]].find(cor) != fc[ind[v]].end() && fc[ind[v]][cor] != 0)
 				fq[ind[v]][fc[ind[v]][cor]] -= cor;
 			fc[ind[v]][cor] += x.snd;
-	//		printf("fc[%d][%d] = %d\n", ind[v], cor, fc[ind[v]][cor]);
 			fq[ind[v]][fc[ind[v]][cor]] += cor;
-	//		printf("fq[%d][%d] %d\n", ind[v], fc[ind[v]][cor], fq[ind[v]][fc[ind[v]][cor]]);
-			mx = max(mx, fc[ind[v]][cor]);
 		}
 	}
-	//printf("junto cor %d em qtd %d\n", c[v], 1);
-//	printf("fc[%d][%d] = %d\n", ind[v], c[v], fc[ind[v]][c[v]]);
-//	printf("fq[%d][%d] %d\n", ind[v], fc[ind[v]][c[v]], fq[ind[v]][fc[ind[v]][c[v]]]);
 
-	res[v] = fq[ind[v]][mx];
+	res[v] = fq[ind[v]].rbegin()->snd;
 }
 
 int main (){
+	deg = 1;
 	scanf("%d", &n);
 	for(int a=0;a<n;a++){
-		scanf("%d", &c[a+1]);
+		scanf("%lld", &c[a+1]);
 	}
 	for(int a=0;a<n-1;a++){
 		int u, v;
@@ -67,6 +61,6 @@ int main (){
 	}
 	go(1, -1);
 	for(int a=1;a<=n;a++){
-		printf("%d ", res[a]);
+		printf("%lld ", res[a]);
 	}
 }
