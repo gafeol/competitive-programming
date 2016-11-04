dccff7dcd3d5926a09ca2c48fb5d10db78f24e69
Gabriel Fernandes <gabfeol@gmail.com>
2016-11-03 23:19:26 -0200

    kd os upsolvs

--- ojs/cf/702/E.cpp
+++ ojs/cf/702/E.cpp
@@ -16,54 +16,43 @@ template<typename T> inline T abs(T t) { return t < 0? -t : t; }
 const ll modn = 1000000007;
 inline ll mod(ll x) { return x % modn; }
 
-const int MAXN = 212345;
+const int MAXN = 212345, LOGN = 35;
 
-int n, m;
+int n, m, nxt[MAXN][LOGN];
 ll k;
-int nxt[MAXN];
-ll w[MAXN];
-int mrk[MAXN];
-vector<int> head;
-ll dc[MAXN];
-int c[MAXN];
+ll sum[MAXN][LOGN], mn[MAXN][LOGN];
 
-vector<int> pil;
-
-void cyc(){
-	c[*pil.rbegin()] = head.size();
-	head.pb(*pil.rbegin());
-	while(
-}
-
-void dfs(int v, int t){
-	if(mrk[v]){
-		if(mrk[v] != t){
-			pil.pop_back();
-			return;
-		}
-		else{
-			cyc();
-		}
-	}
-	mrk[v] = 1;
-	
-
-}
-
-
-int main (){
+int main(){
 	scanf("%d%lld", &n, &k);
 	for(int a=1;a<=n;a++){
-		scanf("%d", &nxt[a]);
-		deg[nxt[a]]++;
+		scanf("%d", &nxt[a][0]);
+		nxt[a][0]++;
 	}
 	for(int a=1;a<=n;a++){
-		scanf("%lld", &w[a]);
+		scanf("%lld", &sum[a][0]);
+		mn[a][0] = sum[a][0];
 	}
-	for(int a=1;a<=n;a++){
-		if(mrk[a] == 0){
-			pil.pb(a);
-			dfs(a, a);
+	ll l;
+	for(l = 1; (1ll<<l) <= k;l++){
+		for(int a=1;a<=n;a++){
+			nxt[a][l] = nxt[nxt[a][l-1]][l-1];
+			sum[a][l] = sum[nxt[a][l-1]][l-1] + sum[a][l-1];
+			mn[a][l] = min(mn[nxt[a][l-1]][l-1], mn[a][l-1]);
 		}
+	}
+	l--;
+	for(int a=1;a<=n;a++){
+		ll kk = k;
+		int v = a;
+		ll S = 0, M = LLONG_MAX;
+		for(ll e = l;e>=0;e--){
+			if((1ll << e) <= kk){
+				kk -= (1ll << e);
+				S += sum[v][e];
+				M = min(M, mn[v][e]);
+				v = nxt[v][e];
+			}
+		}
+		printf("%lld %lld\n", S, M);
 	}
 }
