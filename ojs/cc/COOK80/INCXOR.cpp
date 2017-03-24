11f603552999cb2f2c0fa831786d93f9a9e593ee
Gabriel Fernandes <gabfeol@gmail.com>
2017-03-23 23:53:32 -0300

    o sono vindo

--- ojs/cc/COOK80/INCXOR.cpp
+++ ojs/cc/COOK80/INCXOR.cpp
@@ -22,32 +22,69 @@ const int MAXN = 6, BM = (1<<5) + 10;
 int n, m, k;
 int s[MAXN];
 ll dp[31][BM][BM];
+ll res = 0;
+
+ll go(int bit, int lox, int lo){
+	debug("go(%d %d %d):\n", bit, bm, w);
+	if(bit == -1)
+		return (lo == (1<<(n-1))-1 && lo == lox);
+
+	ll &r = dp[bit][bm][w];
+
+	if(r != -1)
+		return r;
+
+	for(int a=0;a<(1<<n);a++){
+		int fodeu = 0;
+		int bm = 0;
+		for(int i=0;i<n-1;i++){
+			if(lo|(1<<i) && lox|(1<<i)) continue;
+
+			int b = ((a&(1<<i))>>i);
+			int b1 = ((a&(1<<(i+1)))>>(1+i));
+			if((s[i]>>bit)^b > (s[i+1]>>bit)^b1 || b > b1){
+				fodeu = 1;
+				break;
+			}
+			else if((s[i]>>bit)^b < (s[i+1]>>bit)^b1){
+				bm |= (1<<i);
+
+			}
+		}
+		if(!fodeu){
+			debug("	go(%d %d %d) -> go(%d %d %d)\n", bit, bm, w, bit-1, bm, a);
+			r = mod(r + go(bit-1, bm, a)); 
+		}
+	}
+
+}
 
 int main (){
 	for_tests(t, tt){
+		memset(dp, -1, sizeof(dp));
 		scanf("%d", &n);
 		for(int a=0;a<n;a++){
 			scanf("%d", s+a);
 		}
-		ll res = 0;
-		for(int a=0;a<(1<<5);a++){
+		res = 0;
+		for(int a=0;a<(1<<n);a++){
 			int fodeu = 0;
 			int bm = 0;
-			for(int i=0;i<4;i++){
+			for(int i=0;i<n-1;i++){
 				int b = ((a&(1<<i))>>i);
 				int b1 = ((a&(1<<(i+1)))>>(1+i));
-				if((s[a]>>30)^b > (s[a+1]>>30)^b1){
+				if((s[i]>>30)^b > (s[i+1]>>30)^b1){
 					fodeu = 1;
 					break;
 				}
-				else if((s[a]>>30)^b < (s[a+1]>>30)^b1){
-					bm |= 1;
+				else if((s[i]>>30)^b < (s[i+1]>>30)^b1){
+					bm |= (1<<i);
 
 				}
 			}
 			if(!fodeu)
-				go(30, 
+				res = mod(res + go(29, bm, a)); 
 		}
-		res = mod(res + go(30, 
+		printf("%lld\n", res);
 	}
 }
