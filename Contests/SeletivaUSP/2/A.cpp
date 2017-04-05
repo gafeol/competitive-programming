9fbe17f47352c063a1b17b2df0af3a73bc84c0aa
gafeol <gabfeol@gmail.com>
2017-04-05 09:55:26 -0300

    upd

--- Contests/SeletivaUSP/2/A.cpp
+++ Contests/SeletivaUSP/2/A.cpp
@@ -25,13 +25,34 @@ int cross(pii a, pii b){
 	return abs(a.x*b.y - a.y*b.x);
 }
 
+int area(int i, int c, int j){
+	pii va = s[i]-s[c];
+	pii vb = s[j]-s[c];
+
+	return cross(va, vb);
+}
+
 inline int ts(int i, int j){
 	int ii = i - 1, jj = j + 1;
-	while(i < j){
-		int l = (i+j)/3;
-		int r = ((i+j)*2)/3;
-		if(area
+	while(j - i + 1 > 2){
+		int l = i + (j - i + 1)/3;
+		int r = j - (j - i + 1)/3;
+		if(area(ii, l, jj) >= area(ii, r, jj))
+			j = r;
+		else 
+			i = l;
 	}
+	if(area(ii, i, jj) < area(ii, j, jj))
+		swap(i, j);
+	return i;
+}
+
+int deg;
+
+pii cv[MAXN];
+
+bool convex(int i){
+	if(cross(cv[deg-2], cv[deg-1], s[i] >= 0);
 }
 
 int main{
@@ -39,7 +60,16 @@ int main{
 		scanf("%d", &n);
 		for(int a=0;a<n;a++){
 			scanf("%d %d", &s[a].fst, &s[a].snd);
-			s[a+n] = s[a];
+		}
+		sort(s, s+n);
+		cv[deg++] = s[0];
+		cv[deg++] = s[1];
+		for(int a=2;a<n;a++){
+			while(deg > 2 && !convex(a))
+				deg--;
+
+			if(convex(a))
+				cv[deg++] = a;
 		}
 		for(int i=0;i<n;i++){
 			for(int j = i+2;j < n;j++){
