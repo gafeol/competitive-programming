137f4824f66b42494517445cd5ee2fb94b055ede
Gabriel Fernandes <gabfeol@gmail.com>
2017-05-02 23:24:35 -0300

    Fix Ternary Search

--- Contests/SeletivaUSP/3/B.cpp
+++ Contests/SeletivaUSP/3/B.cpp
@@ -22,11 +22,11 @@ int n, m, k;
 ll s[MAXN];
 ll v[MAXN];
 
-long double go(long double t){
-	long double mn  = DBL_MAX;
-	long double mx = DBL_MIN;
+double go(double t){
+	double mn  = 1e15;
+	double mx = -1;
 	for(int a=0;a<n;a++){
-		long double d = (t - s[a])*((long double)v[a]);
+		double d = (t - s[a])*((double)v[a]);
 
 		mn = min(mn, d);	
 		mx = max(mx, d);
@@ -36,16 +36,16 @@ long double go(long double t){
 
 int main (){
 	while(scanf("%d", &n) != EOF && n != 0){
-		clock_t clk = clock();
-		ll mint = LLONG_MIN;
+		double mint = -1;
 		for(int a=0;a<n;a++){
 			scanf("%lld %lld", &s[a], &v[a]);
-			mint = max(mint, s[a]);
+			mint = max(mint, (double)s[a]);
 		}
-		long double i = (long double)mint, j = 1e12;
-		while(j - i > 0.000000001){
-			long double l = i + (j - i)/3.;
-			long double r = j - (j - i)/3.;
+		double i = (double)mint, j = 1e12, eps = 1e-4;
+		while(i < j && j - i > eps){
+			double l = i + (j - i)/((double)3.);
+			double r = j - (j - i)/((double)3.);
+			if(r - l < eps) break;
 			if(go(l) < go(r)){
 				j = r;
 			}
@@ -53,6 +53,6 @@ int main (){
 				i = l;
 			}
 		}
-		printf("%.9Lf\n", go(i));
+		printf("%.9f\n", go(i));
 	}
 }
