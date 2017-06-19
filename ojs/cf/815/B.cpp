36a2177614cf05240cc2d63dc3d553fd1df4f30c
gafeol <gabfeol@gmail.com>
2017-06-19 10:54:36 -0300

    B cf

--- ojs/cf/815/B.cpp
+++ ojs/cf/815/B.cpp
@@ -20,10 +20,69 @@ const int MAXN = 212345;
 
 int n, m, k;
 int s[MAXN];
+int op;
+
+ll fat[MAXN];
+
+ll expo(ll e, ll base){
+	if(e == 0)
+		return 1;
+
+	ll ans = expo(e/2, base);
+	ans = mod(ans*ans);
+
+	if(e&1)
+		ans = mod(ans*base);
+	
+	return ans;
+}
+
+ll inv(ll x){
+	return expo(modn-2, x);
+}
+
+void step(){
+	for(int a=0;a<n-1;a++){
+		s[a] =  s[a] + op*s[a+1];
+		op *= -1;
+	}
+	n--;
+}
+
+void solve(){
+	while(n > 1){
+		step();
+	}
+	printf("%d\n", s[0]);
+	exit(0);
+}
+
+ll bin(ll i, ll j){
+	return mod(mod(fat[i]*inv(fat[i-j]))*inv(fat[j]));
+}
 
 int main (){
+	fat[0] = 1;
+	for(ll a=1;a<MAXN;a++){
+		fat[a] = mod(fat[a-1]*a);
+	}
 	scanf("%d", &n);
 	for(int a=0;a<n;a++){
-
+		scanf("%d", s+a);
 	}
+	int op = 1;
+	if(n < 4)
+		solve();
+	if(n&1) step();	
+	int nos = n/2 - 1;
+	ll A, B;
+	A = B = 0;
+	for(int a=0;a<n;a+=2){
+		A = mod(A + bin(n/2 - 1, a/2)*s[a]);
+	}
+	for(int a=1;a<n;a+=2){
+		B = mod(B + bin(n/2 - 1, a/2)*s[a]);
+	}
+	ll res = mod(A + op*B);
+	printf("%lld\n", mod(res + modn));
 }
