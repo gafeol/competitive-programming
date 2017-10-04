dae47e8e083a971f31b15a2d49fb76ee661233bf
Gabriel Fernandes <gabfeol@gmail.com>
2017-10-04 15:36:32 -0300

    Ups Fluxo J - Students Initiation

--- Contests/NEERC17/J.cpp
+++ Contests/NEERC17/J.cpp
@@ -1,116 +1,142 @@
-#include <cstdio>
-
-#define min(a,b) ((a) < (b) ? (a) : (b))
-
+#include <bits/stdc++.h>
 using namespace std;
+#define fst first
+#define snd second
 typedef unsigned long long ull;
 typedef long long ll;
+typedef pair<int, int> pii;
+#define pb push_back
+#define for_tests(t, tt) int t; scanf("%d", &t); for(int tt = 1; tt <= t; tt++)
+#ifndef ONLINE_JUDGE
+#define debug(args...) //fprintf(stderr,args)
+#else
+#define debug(args...)
+#endif //ONLINE_JUDGE
 template<typename T> inline T abs(T t) { return t < 0? -t : t; }
+const ll modn = 1000000007;
+inline ll mod(ll x) { return x % modn; }
 
-const int N = 5007;
-const int M = 10*N + 7;
-const int oo = 1e9;
+const int MAXN = 5123;
 
-struct Edge {
-	int to, x, u;
-	struct Edge * nx;
-} _E[M], *es = _E;
-typedef struct Edge * edge;
-inline int & x (edge e) { return e->x; }
-inline int & u (edge e) { return e->u; }
-inline edge rev (edge e) { return _E + (1^(e - _E)); }
+int n, m, k;
+pii s[MAXN];
+namespace f{
+	const int maxv = 11234;
+	const int maxe = 4*5123*2;
+	typedef int num;
+	num inf = INT_MAX;
+	int n = maxv;
 
-int n, m;
-int vs, r, s, y[N];
-edge ad[N], hd[N];
+	int to[maxe], en, nx[maxe], es[maxe], lv[maxv], qu[maxv], cr[maxv];
+	num cp[maxe], fl[maxe];
 
-void add (int i, int j){
-	*es = {j, 0, 1, ad[i]}, ad[i] = es++;
-	*es = {i, 0, 0, ad[j]}, ad[j] = es++;
-}
-
-int bfs ()
-{
-	static int q[N];
-	int *qf = q, *qb = q, i;
-	for (i = 0; i < vs; i++) y[i] = oo;
-	y[*qb++ = r] = 0;
-	while (qf != qb){
-		hd[i = *qf++] = ad[i];
-		for (edge e = ad[i]; e; e = e->nx)
-			if (x(e) < u(e) && y[i] + 1 < y[e->to])
-				y[*qb++ = e->to] = y[i] + 1;
-	}
-	return (y[s] < oo);
-}
-
-int dfs (int i, int f)
-{
-	int nf = 0;
-	if (i == s) return f;
-	for (edge e = hd[i]; e = hd[i]; hd[i] = hd[i]->nx)
-		if (x(e) < u(e) && y[e->to] == y[i] + 1
-		&& (nf = dfs (e->to, min (f, u(e) - x(e))))){
-			x(e) += nf, x(rev(e)) -= nf;
-			return nf;
+	bool bfs(int s, int t){
+		memset(lv, -1, sizeof(int)*n);
+		lv[s] = 0;
+		int a = 0, b = 0;
+		qu[b++] = s; cr[s] = es[s];
+		while(a < b){
+			for(int i=es[qu[a]];i != -1;i=nx[i]){
+				if(cp[i] > fl[i] && lv[to[i]] == -1){
+					lv[to[i]] = lv[qu[a]] + 1;
+					qu[b++] = to[i];
+					cr[to[i]] = es[to[i]];
+					if(to[i] == t) return true;
+				}
+			}
+			a++;
 		}
-	return 0;
+		return false;
+	}
+
+	num dfs(int u, int t, num mx){
+		if(u == t) return mx;
+		for(int &i = cr[u];i != -1;i=nx[i])
+			if(cp[i] > fl[i] && lv[to[i]] == lv[u] + 1)
+				if(num a=dfs(to[i], t, min(mx, cp[i] - fl[i]))){
+					fl[i] += a;
+					fl[i^1] -= a;
+					return a;
+				}
+		return 0;
+	}
+
+	num max_flow(int s, int t){
+		num fl = 0, a;
+		while(bfs(s, t))
+			while(a = dfs(s, t, inf))
+				fl += a;
+		return fl;
+	}
+
+	void reset_all(int n2 = maxv){ n = n2; en = 0; memset(es, -1, sizeof(int)*n);}
+	void reset_flow(){ memset(fl, 0, sizeof(num) * en); }
+
+	void add_edge(int a, int b, num c, num rc=0){
+		if(a >= 0 && a<m)
+			debug("es[a] = en %d a %d to b %d c %d\n", en, a, b, c);
+		fl[en] = 0; to[en] = b; cp[en] = c; nx[en] = es[a]; es[a] = en++;
+		if(b >= 0 && b< m)
+			debug("es[b] = en %d to a %d b %d c %d\n", en, a, b, c);
+		fl[en] = 0; to[en] = a; cp[en] = rc; nx[en] = es[b]; es[b] = en++;
+		
+	}
 }
 
-int maxflow (){
-	int ans = 0, nf;
-	while (bfs ())
-		while (nf = dfs (r, oo))
-			ans += nf;
-	return ans;
-}
-
-int pair (int i, int j) { return (i < j) ? i*n + j : pair (j, i); }
-int stu (int i) { return n*n + i; }
-
-int attempt (int k)
-{
-	for (edge e = _E; e < es; e++) x(e) = 0;
-	for (int i = 0; i < n; i++)
-		for (edge e = ad[stu(i)]; e; e = e->nx)
-			if (e->to == s) u(e) = k;
-	return (m <= maxflow());
-}
 
 
 int main (){
-	scanf(" %d%d", &n, &m);
-	vs = n*n + n;
-	r  = vs++; s = vs++;
-	for (int k = 0; k < m; k++){
-		int i, j; scanf (" %d%d", &i, &j); --i; --j;
-		add (pair (i, j), stu (i));
-		add (pair (i, j), stu (j));
-		add (r, pair (i, j));
-	}
-	for (int i = 0; i < n; i++) add (stu (i), s);
+	scanf("%d%d", &n, &m);
+	int S = m + n, T = m+n+1;
+	for(int a=0;a<m;a++){
+		scanf("%d %d", &s[a].fst, &s[a].snd);
+		s[a].fst--;
+		s[a].snd--;
 
-	int lo = 1, hi = n;
-	while (lo < hi){
-		int mid = (lo + hi)/2;
-		if (attempt (mid)) hi = mid;
-		else               lo = 1 + mid;
 	}
-	printf ("%d\n", lo);
-	attempt (lo);
+	int i=0, j = m+1;
+	while(i < j){
+		int mid = (i + j)/2;
+		f::reset_all();
+		f::reset_flow();
+		debug("mid %d\n", mid);
+		for(int a=0;a<m;a++){
+			f::add_edge(S, a, 1);
+			f::add_edge(a, m+s[a].fst, 1);
+			f::add_edge(a, m+s[a].snd, 1);
+		}
+		for(int a=0;a<n;a++)
+			f::add_edge(m+a, T, mid);
+		
+		if(f::max_flow(S, T) == m)
+			j = mid;
+		else
+			i = mid+1;
+	}
+	printf("%d\n", i);
+	f::reset_all();
+	f::reset_flow();
+	for(int a=0;a<m;a++){
+		f::add_edge(S, a, 1);
+		f::add_edge(a, m+s[a].fst, 1);
+		f::add_edge(a, m+s[a].snd, 1);
+	}
+	for(int a=0;a<n;a++)
+		f::add_edge(m+a, T, i);
 
-	for (int i = 0; i < n; i++)
-		for (int j = i + 1; j < n; j++){
-			if (ad[pair (i, j)] == NULL) continue;
-			fprintf (stderr, "[%d %d]\n", 1 + i, 1 + j);
-			for (edge e = ad[pair (i,j)]; e; e = e->nx)
-				if (x(e) == 1 && e->to != r){
-					fprintf (stderr, "[%d]\n", 1 + e->to - n*n); 
-					if (e->to == stu(i))
-						printf ("%d %d\n", 1 + i, 1 + j);
-					else
-						printf ("%d %d\n", 1 + j, 1 + i);
-				}
+	f::max_flow(S, T);
+
+	for(int a=0;a<m;a++){
+		int i = f::es[a];
+		while(i != -1){
+			debug("i %d fl %d cp %d to %d\n", i, f::fl[i], f::cp[i], f::to[i]);
+			if(f::fl[i] == 1){
+				if(f::to[i] == m+s[a].fst)
+					printf("%d %d\n", 1+s[a].fst, 1+s[a].snd);
+				else
+					printf("%d %d\n", 1+s[a].snd, 1+s[a].fst);
+			}
+			i = f::nx[i];
+		}
 	}
 }
-
