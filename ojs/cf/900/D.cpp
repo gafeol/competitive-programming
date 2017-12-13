fb50578950dc55f54eebc19cbb01d647e10b2f26
Gabriel Fernandes <gabfeol@gmail.com>
2017-12-13 00:36:46 -0200

    ac number theory

--- ojs/cf/900/D.cpp
+++ ojs/cf/900/D.cpp
@@ -20,7 +20,6 @@ const int MAXN = 212345;
 
 ll n, m, k;
 vector<ll> pr;
-set<ll> s[MAXN];
 int mrk[MAXN];
 
 ll expo(ll base, ll e){
@@ -32,33 +31,63 @@ ll expo(ll base, ll e){
 	return ans;
 }
 
-int main (){
-	for(ll a=2;a<MAXN;a++){
-		if(mrk[a]) continue;
-		pr.pb(a);
-		for(ll b=1;a*b < MAXN;b++){
-			mrk[a*b]++;
-			s[a*b].insert(a);
+int count(ll x){
+	int cnt =0;
+	ll val = 1;
+	for(int p: pr){
+		if(x%p == 0){
+			cnt++;
+			val *= p;
 		}
 	}
+	return (x == val)*cnt;
+}
+
+map<ll, int> ja;
+
+int main (){
 	scanf("%lld%lld", &n, &m);
 	ll ans = 0;
 	if(m%n == 0){
 		m /= n;
+		ll val = m;
+		for(ll a=2;a<MAXN;a++){
+			if(mrk[a]) continue;
+			if(val%a == 0) 
+				pr.pb(a);
+			while(val%a == 0)
+				val /= a;
+			for(ll b=a;a*b < MAXN;b++)
+				mrk[a*b]++;
+		}
+		if(val != 1)
+			pr.pb(val);
 		ans = expo(2ll, m-1);
+		if(count(m) > 0){
+			if((count(m)&1))
+				ans = mod(ans - 1);
+			else
+				ans = mod(ans + 1);
+		}
 		for(ll p = 2;p<MAXN;p++){
 			if(m%p == 0){
-				debug("mrk %d %d\n", p, mrk[p]); 
-				if(s[p].size() == 1){
-					if(*s[p].begin() == p)
+				if(p > sqrt(m))
+					break;
+				debug("mrk %lld %d\n", p, mrk[p]); 
+				if(count(p) > 0){
+					if((count(p)&1))
 						ans = mod(ans - expo(2ll, m/p - 1)); 
-					continue;
+					else
+						ans = mod(ans + expo(2ll, m/p - 1));
 				}
-				if(((s[p].size())&1)){
-					ans = mod(ans - expo(2ll, m/p - 1)); 
-				}
-				else{
-					ans = mod(ans + expo(2ll, m/p - 1));
+
+				ll o = m/p;
+				if(o <= p) continue;
+				if(count(o) > 0){
+					if((count(o)&1))
+						ans = mod(ans - expo(2ll, m/o - 1)); 
+					else
+						ans = mod(ans + expo(2ll, m/o - 1));
 				}
 			}
 		}
