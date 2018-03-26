059c54be79d8853906a30ed8b8eca099c301c036
Gabriel Fernandes <gabfeol@gmail.com>
2018-03-26 09:21:54 -0300

    Upsolving seletiva

--- Contests/SeletivaUSP/2018/H.cpp
+++ Contests/SeletivaUSP/2018/H.cpp
@@ -8,7 +8,7 @@ typedef pair<int, int> pii;
 #define pb push_back
 #define for_tests(t, tt) int t; scanf("%d", &t); for(int tt = 1; tt <= t; tt++)
 #ifndef ONLINE_JUDGE
-#define debug(args...) fprintf(stderr,args)
+#define debug(args...) //fprintf(stderr,args)
 #else
 #define debug(args...)
 #endif //ONLINE_JUDGE
@@ -19,80 +19,12 @@ inline ll mod(ll x) { return x % modn; }
 const int MAXN = 212345;
 
 int n, m, k;
-int s[MAXN];
 
-vector<int> adj[MAXN], inv[MAXN];
+vector<int> adj[MAXN];
 
-int ind[MAXN], oud[MAXN];
-int prv[MAXN][2], nxt[MAXN];
+int prv[MAXN];
 
-int in[MAXN], mrk[MAXN][2];
-
-int vis[MAXN], cy[MAXN];
-
-queue<int> q;
-
-void put(int u){
-	if(in[u]) return;
-	in[u] = 1;
-	q.push(u);
-}
-
-vector<int> ans;
-
-int inp[MAXN];
-
-void re(int u, int t, int ini){
-	while(u != ini){
-		debug("re u %d ini %d\n", u, ini);
-		ans.pb(u);
-		u = prv[u][t];
-		t = 1 - t;
-	}
-	ans.pb(u);
-}
-
-void go(int u, int t){
-	if(!ans.empty()) return;
-	inp[u] = 1;
-	mrk[u][t] = 1;
-	debug("u %d t %d\n", u, t);
-	for(int nxt: adj[u]){
-		if(cy[nxt] != cy[u]) continue;
-		if(!ans.empty()) return;
-		debug("u %d nxt %d\n", u, nxt);
-		if(mrk[nxt][t] && inp[nxt]) re(nxt, t, u);
-		if(!ans.empty()) return;
-		if(mrk[nxt][1-t]) continue;
-		debug("u %d chama %d\n", u, nxt);
-		prv[nxt][1-t] = u;
-		go(nxt, 1-t);
-	}
-	inp[u] = 0;
-}
-
-int imrk[MAXN][2], iprv[MAXN][2];
-
-void gi(int u, int t, int ini){
-	if(!ans.empty()) return;
-	inp[u] = 1;
-	imrk[u][t] = 1;
-	//if(mrk[u][1-t])
-	//	ire(u, t
-	debug("u %d t %d\n", u, t);
-	for(int nxt: inv[u]){
-		if(cy[nxt] != cy[u]) continue;
-		if(!ans.empty()) return;
-		debug("u %d nxt %d\n", u, nxt);
-	//	if(imrk[nxt][t] && inp[nxt]) ire(nxt, t, u);
-		if(!ans.empty()) return;
-		if(imrk[nxt][1-t]) continue;
-		debug("u %d chama %d\n", u, nxt);
-		iprv[nxt][1-t] = u;
-		gi(nxt, 1-t, ini);
-	}
-	inp[u] = 0;
-}
+int cy[MAXN];
 
 int dege, deg;
 int mn[MAXN], pre[MAXN], mrk2[MAXN];
@@ -120,59 +52,177 @@ int tarjan(int u){
 	return mn[u];
 }
 
+queue<int> qb;
+
+int dt[MAXN];
+int mrkd[MAXN], pred[MAXN];
+
+void dfs(int u){
+	mrkd[u] = 1;
+	for(int nxt: adj[u]){
+		if(mrkd[nxt] || cy[nxt] != cy[u]) continue;
+		pred[nxt] = u;
+		dfs(nxt);
+	}
+}
+
+deque<int> path, ans;
+
+
+void limpapath(){
+	//limpa checando se subciclos sao impar
+	int inp[MAXN];
+	deque<int> auxp;
+	deque<int> aux;
+
+	for(int u: path)
+		inp[u] = 0;
+	for(int u: path){
+		while(inp[u]){
+			inp[auxp.back()] = 0;
+			aux.push_front(auxp.back());
+			auxp.pop_back();
+		}
+
+		if((aux.size()&1)){
+			ans = aux;
+			return;
+		}
+		aux.clear();
+		inp[u] = 1;
+		auxp.push_back(u);
+	}
+	path = auxp;
+	for(int u: path)
+		inp[u] = 0;
+}
+
+void findpath(int u, int v){
+	//assert(cy[u] == cy[v]);
+	//assert(v != 0 && v != -1);
+	pred[u] = u;
+	dfs(u);
+	/*//DEBUG
+	for(int a=1;a<=n;a++){
+		if(cy[a] == cy[u]){
+			assert(mrkd[u]);
+			assert(pred[a] != 0);
+		}
+	}
+	*/
+	debug("find path betw %d and %d\n", u, v);
+	while(v != u){
+		if(v == 0){
+			printf("AAAAAAAAAAAA\n");
+			exit(0);
+		}
+		assert(v != 0 && v != -1);
+		path.push_front(v);
+		v = pred[v];
+	}
+	path.push_front(v);
+	limpapath();
+}
+
+void rootpath(int u, int ini){
+	debug("rootpath %d %d\n", u, ini);
+	if(u != ini){
+		u = prv[u];
+		while(u != ini){
+			assert(u != 0 && u != -1);
+			path.push_front(u);
+			u = prv[u];
+		}
+	}
+	limpapath();
+	if(!ans.empty()) return;
+	ans = path;
+}
+
+void solve(int ini, int u, int v){
+	debug("solve %d %d %d\n", ini, u, v);
+	findpath(v, ini);
+	if(!ans.empty()) return ;
+
+	if((((path.size()-1)&1)^dt[v]))
+		rootpath(v, ini);
+	else{
+		path.push_front(u);
+		rootpath(u, ini);
+	}
+}
+
+void go(int u){
+	debug("go %d\n", u);
+	int ini = u;
+	while(!qb.empty())
+		qb.pop();
+	qb.push(u);	
+	dt[u] = 0;
+	while(!qb.empty()){
+		u = qb.front();
+		qb.pop();
+		for(int nxt: adj[u]){
+			if(cy[nxt] != cy[u]) continue;
+			if(dt[nxt] == 1-dt[u]) continue;
+			else if(dt[nxt] == dt[u]){
+				solve(ini, u, nxt);
+				return ;
+			}
+			prv[nxt] = u;
+			dt[nxt] = 1 - dt[u];
+			qb.push(nxt);
+		}
+	}
+}
+
 int mrkc[MAXN];
+
+void zera(){
+	ans.clear();
+	deg = 0;
+	dege = 0;
+	path.clear();
+	for(int a=0;a<=n;a++){
+		mrk2[a] = 0;
+		cy[a] = -1;
+		prv[a] = 0;
+		mrkc[a] = 0;
+		mrkd[a] = 0;
+		pred[a] = 0;
+		adj[a].clear();
+		dt[a] = -1;
+		pre[a] = 0;
+	}
+}
 
 int main (){
 	for_tests(t, tt){
 		scanf("%d%d", &n, &m);
-		ans.clear();
-		deg = 0;
-		dege = 0;
-		for(int a=1;a<=n;a++){
-			prv[a][0] = 0;
-			prv[a][1] = 0;
-			in[a] = 0;
-			mrk[a][0] = 0;
-			mrk[a][1] = 0;
-			adj[a].clear();
-			inv[a].clear();
-			vis[a] = 0;
-			ind[a] = 0;
-			oud[a] = 0;
-		}
-		while(!q.empty())
-			q.pop();
+		zera();
 		for(int a=0;a<m;a++){
 			int i, j;
 			scanf("%d %d", &i, &j);	
 			adj[i].pb(j);
-			inv[j].pb(i);
+		}
+		for(int a=1;a<=n;a++){
+			if(mrk2[a]) continue;
+			tarjan(a);
 		}
 		for(int a=1;a<=n;a++){
 			if(mrkc[cy[a]]) continue;
 			mrkc[cy[a]] = 1;
-			go(a, 0);
+			go(a);
 			if(!ans.empty()) break;
 		}
-		/*
-		if(ans.empty()){
-			for(int a=1;a<=n;a++){
-				if(mrkc[cy[a]] == 2) continue;
-				mrkc[cy[a]] = 2;
-				gi(a, 0);
-				if(!ans.empty()) break;
-			}
-		}
-		*/
 
 		if(ans.empty())
 			puts("-1");
 		else{
 			puts("1");
 			printf("%d\n", (int)ans.size());
-			for(int a=ans.size()-1;a>=0;a--){
-				printf("%d\n", ans[a]);
-			}
+			for(int a: ans)
+				printf("%d\n", a);
 		}
 	}
 }
