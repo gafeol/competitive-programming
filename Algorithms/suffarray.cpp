9dac50ed3272c352ecd562239f1dbaebb0d506d9
gafeol <gabfeol@gmail.com>
2019-10-16 10:10:49 +0200

    Add suffix array

---
+++ Algorithms/suffarray.cpp
@@ -0,0 +1,47 @@
+
+// Suffix array Nlog2 + Kazai
+
+vector<int> sa(string s){
+    int n = s.size();
+    
+    if(n == 1) // Tratar caso n = 1
+        exit(0);
+
+    vector<int> r(n+1), p(n), aux(n);
+    for(int a=0;a<n;a++){
+        r[a] = s[a];
+        p[a] = a;
+    }
+    r[n] = -1;
+
+    for(int d=1;d < n;d*=2ll){
+        sort(p.begin(), p.end(), [&](int a, int b){
+            return make_pair(r[a], r[min(n, a+d)]) < make_pair(r[b], r[min(n, b+d)]);
+                }); 
+        int val = 0;
+        for(int a=0;a<n;a++){
+            if(a > 0)
+                val += (make_pair(r[p[a]], r[min(n, p[a]+d)]) != make_pair(r[p[a-1]], r[min(n, p[a-1]+d)]));
+            aux[p[a]] = val;
+        }
+        for(int a=0;a<n;a++)
+            r[a] = aux[a];
+        if(r[p[n-1]] == n-1) break;
+    }
+
+    //Kazai 
+    int sz = 0;
+    vector<int> lcp(n); // lcp[i] = |lcp(s[i...], s[p[r[i]+1]....])|
+    for(int i=0;i<n;i++){
+        if(r[i] == n-1){
+            lcp[i] = sz = 0;
+            continue;
+        }
+        int o = p[r[i]+1];
+        while(i + sz < n && o + sz < n && s[i + sz] == s[o + sz])
+            sz++;
+        lcp[i] = sz;
+        sz = max(0, sz-1);
+    }
+    return lcp;
+}
