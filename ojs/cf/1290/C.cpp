8fedd0dad4f51caa2dfdda13ab840348f89fdd5e
gafeol <gabfeol@gmail.com>
2020-09-25 23:22:08 -0300

    Upsolving do melhor problema de union find da terra

--- ojs/cf/1290/C.cpp
+++ ojs/cf/1290/C.cpp
@@ -8,7 +8,7 @@ typedef pair<int, int> pii;
 #define pb push_back
 #define for_tests(t, tt) int t; scanf("%d", &t); for(int tt = 1; tt <= t; tt++)
 #ifndef ONLINE_JUDGE
-#define debug(args...) fprintf(stderr,args)
+#define debug(args...) //fprintf(stderr,args)
 #else
 #define debug(args...)
 #endif //ONLINE_JUDGE
@@ -22,40 +22,97 @@ const int MAXN = 312345;
 int n, m, k;
 int s[MAXN];
 
-vector<vector<int>> bt;
+int p[MAXN], sz[MAXN];
+vector<int> comp[MAXN];
+int c[MAXN][2], state[MAXN];
+vector<int> over[MAXN];
 
-vector<int> over;
-
-int state[MAXN];
-bool ok;
 int ans;
-vector<int> vis;
 
-void go(int u, int t){
-    if(!ok) return;
-    if(state[u] != -1){
-        if(state[u] != t)
-            ok = false;
-        return;
-    }
-
-    state[u] = t;
-    ans += t;
-    vis.pb(u);
-    for(int i: bt[u]){
-        bool exp = (s[a]^t);
-        int cnt = 0;
-        assert(over[i].size() <= 2);
-        if(over[i].size() == 1 && s[i] == t){
-            ok = false;
-            return ;
-        }
-        for(int ot: over[i]){
-            if(ot == u) continue;
-            go(ot, exp);
-        }
-    }
+int raiz(int u){
+    if(p[u] == u) return u;
+    return p[u] = raiz(p[u]);
 }
+
+void inv(int r){
+    for(int ov: comp[r]){
+        state[ov] ^= 1;
+    }
+    swap(c[r][0], c[r][1]);
+}
+int INF = 1e6;
+
+void juntaIgual(int u, int v){
+    debug("junta ig %d %d\n", u, v);
+    int ru = raiz(u);
+    int rv = raiz(v);
+
+    if(ru == rv)
+        return ;
+
+    ans -= min(c[ru][0], c[ru][1]);
+    ans -= min(c[rv][0], c[rv][1]);
+
+    if(sz[ru] < sz[rv])
+        swap(ru, rv);
+    sz[ru] += sz[rv];
+    p[rv] = ru;
+
+    if(state[u] != state[v])
+        inv(rv);
+
+    for(int x: comp[rv])
+        comp[ru].pb(x);
+    comp[rv].clear();
+
+    c[ru][0] += c[rv][0];
+    c[ru][0] = min(c[ru][0], INF);
+    c[ru][1] += c[rv][1];
+    c[ru][1] = min(c[ru][1], INF);
+    ans += min(c[ru][0], c[ru][1]);
+}
+
+void juntaDif(int u, int v){
+    debug("junta dif %d %d\n", u, v);
+    int ru = raiz(u);
+    int rv = raiz(v);
+
+    if(ru == rv)
+        return ;
+
+    ans -= min(c[ru][0], c[ru][1]);
+    ans -= min(c[rv][0], c[rv][1]);
+
+    if(sz[ru] < sz[rv])
+        swap(ru, rv);
+    sz[ru] += sz[rv];
+    p[rv] = ru;
+
+    if(state[u] == state[v])
+        inv(rv);
+
+    for(int x: comp[rv])
+        comp[ru].pb(x);
+    comp[rv].clear();
+
+    c[ru][0] += c[rv][0];
+    c[ru][0] = min(c[ru][0], INF);
+    c[ru][1] += c[rv][1];
+    c[ru][1] = min(c[ru][1], INF);
+    ans += min(c[ru][0], c[ru][1]);
+}
+
+
+void imp(int u, int s){
+    // impossivel ru ser state 
+    int ru = raiz(u);
+    //printf("impossivel u %d s %d ru %d\n", u, s, ru);
+    ans -= min(c[ru][0], c[ru][1]);
+    c[ru][s] = INF;
+    //printf("c[%d] %d %d\n", ru, c[ru][0], c[ru][1]);
+    ans += min(c[ru][0], c[ru][1]);
+}
+
 
 int main (){
 	scanf("%d%d", &n, &m);
@@ -65,54 +122,38 @@ int main (){
         s[a] = c-'0';
 	}
     for(int a=0;a<m;a++){
-        scanf("%d", &q);
-        vector<int> chg;
-        while(q--){
-            int x;
-            scanf("%d", &x); 
-            x--;
-            chg.pb(x);
-            over[x].pb(a);
+        int x;
+        scanf("%d", &x);
+        p[a] = a;
+        sz[a] = 1;
+        comp[a].pb(a);
+        state[a] = 0;
+        c[a][0] = 1;
+        c[a][1] = 0;
+        while(x--){
+            int u;
+            scanf("%d", &u);
+            u--;
+            over[u].pb(a);
         }
-        bt.pb(chg);
     }
-    int res = 0;
-    memset(state, -1, sizeof(state));
     ans = 0;
-    for(int a=0;a<m;a++){
-        bool ok0, ok1;
-        ok0 = ok1 = false;
-        int ans0, ans1;
-        ans0 = ans1 = 0;
-        if(state[a] == -1){
-            vis.clear();
-            ok = true;
-            ans = 0;
-            go(a, 0);
-            ok0 = ok;
-            ans0 = ans;
-            for(int u: vis){
-                state[u] = -1;
-            }
-            vis.clear();
-            ok = true;
-            ans = 0;
-            go(a, 1);
-            ok1 = ok;
-            ans1 = ans;
-
-
-            for(int u: vis)
-                state[u] = -1;
-            vis.clear();
-            ok = true;
-            ans = 0;
-            if(ok0 && ok1){
-                if(ans0 < ans1){
-                    go();
-                }
-            }
+    for(int i=0;i<n;i++){
+        if(over[i].size() == 2){
+            int u = over[i][0], v = over[i][1];
+            if(s[i])
+                juntaIgual(u, v);
+            else
+                juntaDif(u, v);
         }
+        else if(over[i].size() == 1){
+            int u = over[i][0];
+            if(s[i])
+                imp(u, state[u]);
+            else
+                imp(u, 1-state[u]);
+
+        }
+        printf("%d\n", ans);
     }
 }
-
