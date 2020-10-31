c5934fb2ad0ba344bc6893cba79743036964652a
gafeol <gabfeol@gmail.com>
2020-10-31 00:10:53 -0300

    adds vasito pers seg, chg treap

--- Algorithms/treap.cpp
+++ Algorithms/treap.cpp
@@ -3,10 +3,10 @@ num X[N]; int en = 1, Y[N], sz[N], L[N], R[N];
 void calc (int u) { // update node given children info
 	sz[u] = sz[L[u]] + 1 + sz[R[u]];
 	// code here, no recursion
+    // Talvez precise chamar unlaze de u e dos filhos, pra garantir que voce vai conseguir calcular os parametros que quer com os filhos atualizados
 }
 void unlaze (int u) {
 	if(!u) return;
-	// code here, no recursion
 }
 void split_val(int u, num x, int &l, int &r) { // l gets <= x, r gets > x
 	unlaze(u); if(!u) return (void) (l = r = 0);
@@ -14,10 +14,10 @@ void split_val(int u, num x, int &l, int &r) { // l gets <= x, r gets > x
 	else { split_val(L[u], x, l, r); L[u] = r; r = u; }
 	calc(u);
 }
-void split_sz(int u, int s, int &l, int &r) { // l gets first s, r gets remaining
+void split_sz(int u, int S, int &l, int &r) { // l gets first s, r gets remaining
 	unlaze(u); if(!u) return (void) (l = r = 0);
-	if(sz[L[u]] < s) { split_sz(R[u], s - sz[L[u]] - 1, l, r); R[u] = l; l = u; }
-	else { split_sz(L[u], s, l, r); L[u] = r; r = u; }
+	if(sz[L[u]] < S) { split_sz(R[u], S - sz[L[u]] - 1, l, r); R[u] = l; l = u; }
+	else { split_sz(L[u], S, l, r); L[u] = r; r = u; }
 	calc(u);
 }
 int merge(int l, int r) { // els on l <= els on r
